d71bec14993df2268b5084ad57a8648389ddd11b
cryptodoctor <emmanuelneni@gmail.com>
2019-07-03 14:16:41 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <productList/>
+  </div>
+</template>
+
+<script>
+import productList from './components/productList'
+
+export default {
+  name: 'app',
+  components: {
+    productList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vuex from "vuex"
+import Vue from 'vue';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    products: []
+
+  },
+
+  getters: {
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0)
+    }
+  },
+
+  actions: {
+    fetchProducts () {
+
+    }
+  },
+   mutations: {
+     setProducts (state, products ) {
+       state.products = products
+
+     }
+   }
+})
